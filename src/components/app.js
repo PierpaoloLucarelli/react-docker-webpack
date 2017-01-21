a90cd7318b769a21b9b137f1217b471d8b54f1d1
Pierpaolo <plucarelli.94@gmail.com>
2017-01-21 16:01:43 +0000

    added login test

--- src/components/app.js
+++ src/components/app.js
@@ -1,52 +1,22 @@
 import React from 'react';
-import CreateTodo from './create_todo';
-import TodoList from './todo_list';
-
-const todos = [
-    {
-        task: "Make breakfast",
-        isCompleted: false
-    },
-    {
-        task: "Eat dinner",
-        isCompleted: true
-    }
-];
+import SignupForm from './signup';
 
 export default class App extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            todos
+            items: []
         };
     }
 
     render(){
         return(
             <div>
-                <h1>Hello from Alydo</h1>
-                <CreateTodo createTask={this.createTask.bind(this)}/>
-                <TodoList toggleTask={this.toggleTask.bind(this)}
-                    todos={this.state.todos}
-                />
+            <h1>Alydo</h1>
+            <SignupForm/>
             </div>
         )
     }
 
-    createTask(task){
-        this.state.todos.push({
-            task,
-            isCompleted: false
-        });
-        this.setState({
-            todos: this.state.todos
-        })
-    }
-
-    toggleTask(task){
-        const foundTodo = _.find(this.state.todos, todo => todo.task === task);
-        foundTodo.isCompleted = ! foundTodo.isCompleted;
-        this.setState({todos: this.state.todos});
-    }
 }
---
+++ src/components/signup.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+export default class SignupForm extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            username: null,
+            password: null
+        }
+    }
+
+    render(){
+        return(
+            <div className="signup">
+                <h1>Create an account with Alydo</h1>
+                <form className="alydo-form" onSubmit={this.sendRegister.bind(this)}>
+                <input
+                    ref="username"
+                    type="text"
+                    placeholder="Username"
+                    defaultValue={this.state.email}
+                    onChange={this.handleUsername.bind(this)}
+                 />
+
+                 <input
+                    ref="password"
+                    type="password"
+                    placeholder="Password"
+                    defaultValue={this.state.password}
+                    onChange={this.handlePassword.bind(this)}
+                 />
+
+                 <input
+                    ref="passwordConfirm"
+                    type="password"
+                    placeholder="Confirm password"
+                    defaultValue={this.state.password}
+                 />
+
+                 <input
+                    type="submit"
+                    className="button red-btn"
+                    onChange={this.handleConfirmPassword.bind(this)}
+                />
+
+                </form>
+            </div>
+        );
+    }
+
+    handleUsername(event){
+        this.setState({
+            username: event.target.value
+        });
+    }
+
+    handleConfirmPassword(event){
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    handlePassword(event){
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    sendRegister(event){
+        event.preventDefault();
+        fetch("http://127.0.0.1:8080/signup", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password,
+            })
+        })
+    }
+}
